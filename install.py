d1e80a2040cdc007ab805576348abeb4a7979116
LimitlessGreen <limitlessgreen@outlook.de>
2019-04-22 17:12:02 +0200

    initial commit

---
+++ install.py
@@ -0,0 +1,15 @@
+# weapi installer
+
+from setup import ExtensionInstaller
+
+class weapiInstallerExtensionInstaller):
+    def __init__(self):
+        super(weapiInstaller, self).__init__(
+            version="0.0.1",
+            name='weapi',
+            description='Nothing to say here yet',
+            author="LimitlessGreen, severusstick",
+            files=[('bin', ['bin/weapi.py'])]
+
+
+        )
