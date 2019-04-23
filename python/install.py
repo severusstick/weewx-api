8770e2335a1cfd282b4b4775d16c51cdf1ea7076
LimitlessGreen <limitlessgreen@outlook.de>
2019-04-23 16:46:23 +0200

    Update setup.py

--- python/install.py
+++ python/install.py
@@ -1,15 +1,16 @@
 # weapi installer
 
-from setup import ExtensionInstaller
+from weecfg.extension import ExtensionInstaller
 
-class weapiInstallerExtensionInstaller):
+def loader():
+    return weapiInstaller()
+
+class weapiInstaller(ExtensionInstaller):
     def __init__(self):
         super(weapiInstaller, self).__init__(
             version="0.0.1",
             name='weapi',
             description='Nothing to say here yet',
             author="LimitlessGreen, severusstick",
-            files=[('bin', ['bin/weapi.py'])]
-
-
+            files=[('bin/user', ['bin/user/weapi.py'])]
         )
