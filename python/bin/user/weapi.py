0107726e20ca41c0196c07eb84a8e56c0fdc5d88
LimitlessGreen <limitlessgreen@outlook.de>
2019-04-23 18:19:21 +0200

    weapi | add function to get config

--- python/bin/user/weapi.py
+++ python/bin/user/weapi.py
@@ -1,1 +1,17 @@
-# some comment
+from weewx.restx import StdRESTful, get_site_dict
+import weewx.manager
+
+class StdApi(StdRESTful):
+
+    _server = None
+
+    def __init__(self, engine, config_dict):
+
+        super(StdApi, self).__init__(engine, config_dict)
+
+        site_dict = get_site_dict(config_dict, 'WeAPI', 'url')
+
+        if site_dict is None:  # return if restful API is disabled
+            return
+
+        self._server = site_dict['url']
