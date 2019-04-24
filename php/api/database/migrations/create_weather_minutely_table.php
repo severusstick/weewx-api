f6b268b9c6049b5357322a9f81067723fd765a6e
LimitlessGreen <limitlessgreen@outlook.de>
2019-04-25 00:11:04 +0200

    add database migration for minutely_updates

---
+++ php/api/database/migrations/create_weather_minutely_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWeatherMinutelyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weather_minutely', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('dateTime');
+            $table->double('barometer');
+            $table->double('outTemp');
+            $table->double('outHumidity');
+            $table->double('windSpeed');
+            $table->double('windDir');
+            $table->double('windGust');
+            $table->double('windGustDir');
+            $table->double('dewpoint');
+            $table->double('hourRain');
+            $table->double('dayRain');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weather_minutely');
+    }
+}
