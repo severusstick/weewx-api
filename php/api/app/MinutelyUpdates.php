b0c3b0368be2d4aae2c73216e5b54c33dbfdf633
LimitlessGreen <limitlessgreen@outlook.de>
2019-04-25 11:51:56 +0200

    add MinutelyUpdates model

---
+++ php/api/app/MinutelyUpdates.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MinutelyUpdates extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'dateTime', 
+        'barometer',
+        'outTemp',
+        'outHumidity',
+        'windSpeed',
+        'windDir',
+        'windGust',
+        'windGustDir',
+        'dewpoint',
+        'hourRain',
+        'dayRain'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
