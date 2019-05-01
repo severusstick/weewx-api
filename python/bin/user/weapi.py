a5c079bea7a9086fca92fc4399d3418018e317fe
LimitlessGreen <limitlessgreen@outlook.de>
2019-05-01 17:14:12 +0200

    python weapi extension | first working version

--- python/bin/user/weapi.py
+++ python/bin/user/weapi.py
@@ -3,6 +3,8 @@ import weewx.manager
 import Queue
 import syslog
 import sys
+import urllib2
+import urllib
 
 
 class StdApi(weewx.restx.StdRESTful):
@@ -11,21 +13,22 @@ class StdApi(weewx.restx.StdRESTful):
     def __init__(self, engine, config_dict):
         super(StdApi, self).__init__(engine, config_dict)
 
-        print("\n\nTEST\n\n")
         site_dict = weewx.restx.get_site_dict(config_dict, 'WeAPI', 'url')
 
         if site_dict is None:  # return if restful API is disabled
             return
 
-        site_dict.setdefault('latitude', engine.stn_info.latitude_f)
-        site_dict.setdefault('longitude', engine.stn_info.longitude_f)
-        site_dict.setdefault('language', 'de')
-
-        self._server = site_dict['url']
-
         # Get the database manager dictionary:
         site_dict['manager_dict'] = weewx.manager.get_manager_dict_from_config(
             config_dict, 'wx_binding')
+
+        site_dict.setdefault('latitude', engine.stn_info.latitude_f)
+        site_dict.setdefault('longitude', engine.stn_info.longitude_f)
+        site_dict.setdefault('station_type', config_dict['Station'].get(
+            'station_type', 'Unknown'))
+
+        self._server = site_dict['url']
+
 
         self.archive_queue = Queue.Queue()
 
@@ -42,15 +45,12 @@ class StdApi(weewx.restx.StdRESTful):
 
 
 class WEAPIThread(weewx.restx.RESTThread):
-    def __init__(self,
-                 queue,
-                 manager_dict,
+    def __init__(self, queue, manager_dict,
                  url,
-                 station,
-                 post_interval=None, max_backlog=sys.maxint, stale=None,
+                 latitude, longitude, station_type,
+                 post_interval=600, max_backlog=sys.maxint, stale=600,
                  log_success=True, log_failure=True,
-                 timeout=10, max_tries=3, retry_wait=5, retry_login=3600,
-                 skip_upload=False):
+                 timeout=10, max_tries=3, retry_wait=5, skip_upload=False):
 
         super(WEAPIThread, self).__init__(queue,
                                           protocol_name="WEAPI",
@@ -63,5 +63,27 @@ class WEAPIThread(weewx.restx.RESTThread):
                                           skip_upload=False)
 
         self.server_url = url
+
+    def process_record(self, record, dbmanager):
+        """Default version of process_record.
+
+        This version uses HTTP GETs to do the post, which should work for many
+        protocols, but it can always be replaced by a specializing class."""
+
+        # Get the full record by querying the database ...
+        _full_record = self.get_record(record, dbmanager)
+
+        syslog.syslog(syslog.LOG_INFO, "restx: WEAPI: "
+                                       "Data posted to %s" % self.server_url)
+        # ... check it ...
+        self.check_this_record(_full_record)
+        # ... get the Request obj to go with it...
+        _request = self.get_request(self.server_url)
+        #  ... get any POST payload...
+
+        _request.add_data(urllib.urlencode(_full_record))
+
+        self.post_with_retries(_request)
+
 
 
